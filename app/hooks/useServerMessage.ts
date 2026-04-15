82b33dd3043bfdfc0bee1fa1931096b61ee64194
abbouformations <135717843+abbouformations@users.noreply.github.com>
2026-04-15 18:43:32 +0100

    Update useServerMessage.ts

--- app/hooks/useServerMessage.ts
+++ app/hooks/useServerMessage.ts
@@ -2,9 +2,11 @@
 
 import { useEffect } from "react";
 import { useMessage } from "../providers/MessageProvider";
+import { useRouter } from "next/navigation";
 
 export function useServerMessage(state: any) {
   const { addMessage } = useMessage();
+  const router = useRouter();
 
   useEffect(() => {
     if (!state) return;
@@ -12,5 +14,8 @@ export function useServerMessage(state: any) {
     if (state.error) addMessage(state.error, "error");
     if (state.success) addMessage(state.success, "success");
     if (state.info) addMessage(state.info, "info");
+    if (state?.redirectTo) {
+      router.push(state.redirectTo);
+    }
   }, [state]);
 }
